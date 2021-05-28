5ac35cebd90145c17630056607e8ddf0ee6decd5
Felix Larsen <i4guar@gmail.com>
2021-05-28 13:26:09 +0200

    Initial Commit

---
+++ LocalTaskList/LocalTaskListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalTaskListApp.swift
+//  LocalTaskList
+//
+//  Created by Felix Houghton-Larsen on 28.05.21.
+//
+
+import SwiftUI
+
+@main
+struct LocalTaskListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
